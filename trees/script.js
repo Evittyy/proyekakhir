6755594eff4ba6702e57a88acf8b0e1ee9d2c77d
Evittyy <mandytwilight.ala@gmail.com>
2025-02-27 20:38:36 +0700

    Update script.js

--- trees/script.js
+++ trees/script.js
@@ -18,12 +18,19 @@ function handleNoClick() {
     const yesButton = document.querySelector('.yes-button');
     noButton.textContent = messages[messageIndex];
     messageIndex = (messageIndex + 1) % messages.length;
-    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
-    yesButton.style.fontSize = ${currentSize * 1.5}px;
+    currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = currentSize * 1.5 + "px";
 }
 
 function handleYesClick() {
     window.location.href = "yes_page.html";
 }
 
+function handleknowmore() {
+    window.location.href = "more.html";
+}
+
+function handlegoback() {
+    window.location.href = "index.html";
+}
 
